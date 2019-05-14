931a1e9e465f13e518f3db7381aef55e63576555
djffk124 <49220536+djffk124@users.noreply.github.com>
2019-05-14 19:13:52 +0900

    final

---
+++ finalproject/Js/main.js
@@ -0,0 +1,6 @@
+var body = document.createElement('body')
+var menu = document.querySelector('menu')
+var userinfo = document.querySelector('userinfo')
+var cheat = document.querySelector('cheat')
+var qna = document.querySelector('qna')
+
