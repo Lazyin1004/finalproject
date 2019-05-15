674daf2031f07b5e546763df68f4e19a4c0c8e9f
djffk124 <49220536+djffk124@users.noreply.github.com>
2019-05-15 17:14:36 +0900

    plz

--- finalproject/Js/main.js
+++
@@ -1,6 +0,0 @@
-var body = document.createElement('body')
-var menu = document.querySelector('menu')
-var userinfo = document.querySelector('userinfo')
-var cheat = document.querySelector('cheat')
-var qna = document.querySelector('qna')
-
